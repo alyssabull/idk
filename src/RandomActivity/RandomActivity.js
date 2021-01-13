20c426f466fedd706061d2a4b8ea2589ceae3c5d
Alyssa Bull <alyssabull@gmail.com>
2021-01-13 14:57:58 -0700

    Refresh save activity button when a new activity is displayed

--- src/RandomActivity/RandomActivity.js
+++ src/RandomActivity/RandomActivity.js
@@ -5,17 +5,6 @@ const RandomActivity = (props) => {
   const [savedActivities, setSavedActivities] = useState([])
   const [savedStatus, setSavedStatus] = useState('+ Save Activity')
 
-  const toggleActivity = (currentActivity) => {
-    if (savedStatus === '+ Save Activity') {
-      setSavedActivities([...savedActivities, currentActivity])
-    } else if (savedStatus === 'Activity Saved!') {
-      const filteredActivities = savedActivities.filter(activity => {
-        return activity.key !== currentActivity.key
-      })
-      setSavedActivities(filteredActivities)
-    }
-  }
-
   useEffect(() => {
     const determineSavedStatus = savedActivities.filter(activity => {
         return activity.key === props.randomActivity.key
@@ -26,6 +15,21 @@ const RandomActivity = (props) => {
         setSavedStatus('+ Save Activity')
       }
   }, [savedActivities])
+
+  useEffect(() => {
+    setSavedStatus('+ Save Activity')
+  }, [props])
+
+  const toggleActivity = (currentActivity) => {
+    if (savedStatus === '+ Save Activity') {
+      setSavedActivities([...savedActivities, currentActivity])
+    } else if (savedStatus === 'Activity Saved!') {
+      const filteredActivities = savedActivities.filter(activity => {
+        return activity.key !== currentActivity.key
+      })
+      setSavedActivities(filteredActivities)
+    }
+  }
 
   return(
     <section>
