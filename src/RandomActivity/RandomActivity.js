380ebfb6329556dc348e90485f5e969602a90e85
Alyssa Bull <alyssabull@gmail.com>
2021-01-13 14:53:38 -0700

    Add callback function to eliminate infinite renders

--- src/RandomActivity/RandomActivity.js
+++ src/RandomActivity/RandomActivity.js
@@ -5,9 +5,14 @@ const RandomActivity = (props) => {
   const [savedActivities, setSavedActivities] = useState([])
   const [savedStatus, setSavedStatus] = useState('+ Save Activity')
 
-  const saveActivity = (currentActivity) => {
+  const toggleActivity = (currentActivity) => {
     if (savedStatus === '+ Save Activity') {
       setSavedActivities([...savedActivities, currentActivity])
+    } else if (savedStatus === 'Activity Saved!') {
+      const filteredActivities = savedActivities.filter(activity => {
+        return activity.key !== currentActivity.key
+      })
+      setSavedActivities(filteredActivities)
     }
   }
 
@@ -17,13 +22,15 @@ const RandomActivity = (props) => {
       })
       if (determineSavedStatus.length > 0) {
         setSavedStatus('Activity Saved!')
-      }  
+      } else {
+        setSavedStatus('+ Save Activity')
+      }
   }, [savedActivities])
 
   return(
     <section>
       <h1>{props.randomActivity.activity}</h1>
-      <button onClick={() => saveActivity(props.randomActivity)}>{savedStatus}</button>
+      <button onClick={() => toggleActivity(props.randomActivity)}>{savedStatus}</button>
       <button onClick={props.generateNewActivity}>Show New Activity</button>
       {props.randomActivity.link !== '' && <button>Get Started Here!</button>}
     </section>
