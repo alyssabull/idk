acafe7d67d4888a377a36421dc814cee0a3b97c2
Alyssa Bull <alyssabull@gmail.com>
2021-01-14 21:48:29 -0700

    Add icons to saved activities cards

--- src/ActivityCard/ActivityCard.js
+++ src/ActivityCard/ActivityCard.js
@@ -1,10 +1,41 @@
+import { BsPeopleFill } from 'react-icons/bs'
+import { MdSchool, MdWork } from 'react-icons/md'
+import { AiFillPhone } from 'react-icons/ai'
+import { FaHammer, FaBath, FaMusic, FaHiking } from 'react-icons/fa'
+import { BiDonateHeart } from 'react-icons/bi'
+import { GiCookingPot } from 'react-icons/gi'
 import './ActivityCard.scss'
 
 const ActivityCard = (props) => {
+  const determineIcon = () => {
+    if (props.type === 'education') {
+      return <MdSchool size={20} />
+    } else if (props.type === 'recreational') {
+      return <FaHiking size={20} />
+    } else if (props.type === 'social') {
+      return <AiFillPhone size={20} />
+    } else if (props.type === 'diy') {
+      return <FaHammer size={20} />
+    } else if (props.type === 'charity') {
+      return <BiDonateHeart size={20} />
+    } else if (props.type === 'cooking') {
+      return <GiCookingPot size={20} />
+    } else if (props.type === 'relaxation') {
+      return <FaBath size={20} />
+    } else if (props.type === 'music') {
+      return <FaMusic size={20} />
+    } else if (props.type === 'busywork') {
+      return <MdWork size={20} />
+    }
+  }
+
   return(
     <section className='single-activity-card'>
       <h1 className='title'>{props.activity}</h1>
-      <p className='type'>{props.type}</p>
+      <section className='card-details'>
+        <p className='type'>{determineIcon()} {props.type}</p>
+        <p className='participants'><BsPeopleFill size={20}/> {props.participants}</p>
+      </section>
       <button onClick={() => props.deleteSavedActivity(props.id)} className='remove-button'>REMOVE ACTIVITY</button>
     </section>
   )
